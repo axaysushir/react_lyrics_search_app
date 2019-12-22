f3ba09fe3424775dc672fb40c145e5bdc04f4253
Axay Sushir <sinewave_11@yahoo.com>
2019-12-22 15:28:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Index from "./components/layout/Index";
+import Lyrics from "./components/tracks/Lyrics";
+
+import {Provider} from './context';
+// import Lyrics from "./components/tracks/Lyrics";
+
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+      <Router>
+      <React.Fragment>
+        <Navbar />
+        <div className='container'>
+          <Switch>
+            <Route exact path='/' component={Index} />
+            <Route exact path='/lyrics/track/:id' component={Lyrics} />
+
+          </Switch>
+        </div>
+      </React.Fragment>
+      </Router>
+      </Provider>
+    )
+  }
+}
+
+export default App;
